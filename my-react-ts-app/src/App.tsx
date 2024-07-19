7e92085397816a7e0daff445be14548ebdf838c2
vsevolodepihin <v.epihin@tg.dunice.net>
2024-07-19 12:53:40 +0300

    initial commit

---
+++ my-react-ts-app/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        hello
+      </div>
+
+    </>
+  )
+}
+
+export default App
